85b7a6382468f0b8a302bd2e22f62cd9f21c14be
yechiel <51932344+yechielb2000@users.noreply.github.com>
2022-10-30 22:57:11 +0200

    Making it readable for change 🤗

--- game.py
+++ game.py
@@ -1,12 +1,13 @@
+from typing import List
 import threading 
 import socket
+
 
 
 class Game:
 
     def __init__(self) -> None:
         self.format = "utf-8"
-
         self.board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "]]
         self.turn = "X"
         self.you = "X"
@@ -15,30 +16,21 @@ class Game:
         self.game_over = False
         self.counter = 0
 
-    def host_game(self, host, port) -> None:
-        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind((host, port))
-        server.listen(1)
+    def host_game(self, host: str, port: int) -> None:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
+            server.bind((host, port))
+            server.listen(1)
+            client, addr = server.accept()
+            threading.Thread(target=self.game_loop, args=(client,)).start()
 
-        client, addr = server.accept()
-
-        self.you = "X"
-        self.opponent = "O"
-
-        threading.Thread(target=self.handle_connection, args=(client,)).start()
-        server.close()
-
-    def connect_to_game(self, host, port) -> None:    
-        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    def connect_to_game(self, host: str, port: int) -> None:    
+        client = socket.socket()
         client.connect((host, port))
-
         self.you = "O"
         self.opponent = "X"
-        threading.Thread(target=self.handle_connection, args=(client,)).start()
+        threading.Thread(target=self.game_loop, args=(client,)).start()
 
-    def handle_connection(self, client) -> None:
+    def game_loop(self, client: socket.SocketType) -> None:
         print("Game is on!")
         while not self.game_over:       
             if self.turn == self.you:
@@ -58,7 +50,7 @@ class Game:
                     self.apply_move(data.decode(self.format).split(','), self.opponent, client)  
                     self.turn = self.you
 
-    def apply_move(self, move, player, client) -> None:
+    def apply_move(self, move: List[str], player: str, client: socket.SocketType) -> None:
         if self.game_over:
             return
         self.counter += 1
@@ -75,31 +67,31 @@ class Game:
                 print("It's a tie!")
                 exit()                                     
 
-    def check_valid_move(self, move) -> bool:
+    def check_valid_move(self, move: List[str]) -> bool:
         return self.board[int(move[0])][int(move[1])] == " "
 
     def check_if_won(self) -> bool:
-            for row in range(3):
-                if self.board[row][0] == self.board[row][1] == self.board[row][2] == self.you: 
-                    self.winner = self.you
-                    self.game_over = True
-                    return True
-
-            for col in range(3):
-                if self.board[0][col] == self.board[1][col] == self.board[2][col] == self.you: 
-                    self.winner = self.you
-                    self.game_over = True
-                    return True        
-
-            if self.board[0][0] == self.board[1][1] == self.board[2][2] == self.you:         
+        for row in range(3):
+            if self.board[row][0] == self.board[row][1] == self.board[row][2] == self.you: 
                 self.winner = self.you
                 self.game_over = True
-                return True   
+                return True    
 
-            if self.board[0][2] == self.board[1][1] == self.board[2][0] == self.you:         
+        for col in range(3):
+            if self.board[0][col] == self.board[1][col] == self.board[2][col] == self.you: 
                 self.winner = self.you
                 self.game_over = True
                 return True        
+
+        if self.board[0][0] == self.board[1][1] == self.board[2][2] == self.you:         
+            self.winner = self.you
+            self.game_over = True
+            return True   
+
+        if self.board[0][2] == self.board[1][1] == self.board[2][0] == self.you:         
+            self.winner = self.you
+            self.game_over = True
+            return True        
 
     def print_board(self) -> None:
         for row in range(3):
--- menu.py
+++ menu.py
@@ -1,15 +1,26 @@
+#!usr/bin/env python3
+
 import game
 
 
-
-if __name__ == "__main__":
+def main() -> None:
     game = Game()
+    connect_to_game()      
+    
 
-    action = ""
-    while action != "1" and action != "2":
-        action = input("1 to join a game 2 to create new one : ")
-
-    if int(action) == 1:
+def connect_to_game() -> None:
+    if get_action() == 1:
         game.connect_to_game("localhost", 9999)
     else:
         game.host_game("localhost", 9999)    
+
+
+def get_action() -> str:
+    action = ""
+    while action != "1" and action != "2":
+        action = input("1 to join a game 2 to create new one : ")
+    return int(action)
+
+
+if __name__ == "__main__":
+    main()    
